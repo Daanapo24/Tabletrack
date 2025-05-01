274842e7433eee9309b25a20991c34265891ed5c
Daanapo24 <daanhamstra@live.nl>
2025-05-01 11:54:22 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-function editName(player) {
-    const newName = prompt("Enter player name:");
-    if (newName) {
-        document.querySelector(`#score${player}`).nextElementSibling.textContent = `✏️ ${newName}`;
-    }
-}
